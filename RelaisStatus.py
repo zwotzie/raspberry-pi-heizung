25401f86eea0177359214da4e46f4efcbdc84ab1
Ralf Hense <rh@web.de>
2021-04-23 08:59:26 +0200

    add relais status for checking

---
+++ RelaisStatus.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import platform
+
+raspberry = False
+if 'raspberrypi' in platform.uname():
+    # global raspberry
+    raspberry = True
+    import RPi.GPIO as GPIO
+
+    # gpio 4 = BCM 23 = Pin 16
+    RelaisHeizung = 23
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+
+GPIO.setup(RelaisHeizung, GPIO.IN)
+state = GPIO.input(RelaisHeizung)
+
+if state == 0:
+    print("Heizungsrelais ist aus")
+elif state == 1:
+    print("Heizungsrelais ist an")
